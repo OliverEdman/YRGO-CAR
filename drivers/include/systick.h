53041b210a1406f124d42b50b897270ab2206f72
Oliver Edman Chrantz <o.edman@icloud.com>
2026-08-28 20:21:41 +0200

    add headerfile for the systick timer

---
+++ drivers/include/systick.h
@@ -0,0 +1,35 @@
+/**
+ * @brief SysTick timer driver for ARM Cortex-M4 (STM32F446).
+ *
+ * Provides non-blocking system tick accounting and millisecond delay 
+ * functionality using the dedicated core SysTick timer.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+/**
+ * @brief Initializes the ARM Cortex-M4 SysTick timer.
+ *
+ * Configures the reload register based on the system clock frequency to 
+ * generate an interrupt every 1 millisecond.
+ *
+ * @param sys_clk_hz Core clock frequency in Hz (Example: 16000000UL for 16 MHz).
+ */
+void systick_init(uint32_t sys_clk_hz);
+
+/**
+ * @brief Gets the elapsed time since system boot.
+ *
+ * @return Current tick count in milliseconds.
+ */
+uint32_t systick_get_ticks(void);
+
+/**
+ * @brief Interrupt Service Routine (ISR) for the SysTick timer.
+ *
+ * Automatically triggered every 1 ms by hardware. Increments the internal 
+ * millisecond tick counter ++.
+ */
+void SysTick_Handler(void);
